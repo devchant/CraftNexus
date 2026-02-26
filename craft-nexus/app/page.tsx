ce2fd931c14ba0de41635362bcd74fed85b9dcec
Agbeleshe <danielagbeleshe@gmail.com>
2026-02-26 09:57:15 +0100

    local changes

--- craft-nexus/app/page.tsx
+++ craft-nexus/app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/organisms/HomepageHero";
 import TestimonialsSection from "@/components/organisms/Testimonials";
+import FeatureBar from "@/components/organisms/FeatureBar";
 
 export default function Home() {
   return (
     <div className="">
       <HeroSection />
+      <FeatureBar />
       <TestimonialsSection />
     </div>
 
